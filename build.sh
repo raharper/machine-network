2669ab54a86cd1a18b61ead95ea0ba34af9ec6ef
Ryan Harper <rharper@woxford.com>
2023-03-08 15:18:40 -0600

    use bootkit minbase

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 TOP_D=$PWD
 BUILD_D=${TOP_D}/build
 PASST_VERSION="2023_02_22.4ddbcb9"
+DOCKER_BASE="docker://"
+UBUNTU_MIRROR="http://archive.ubuntu.com/ubuntu"
 
 stacker --debug --storage-type=overlay \
 	"--oci-dir=$BUILD_D/oci" "--roots-dir=$BUILD_D/roots" "--stacker-dir=$BUILD_D/stacker" \
@@ -10,4 +12,6 @@ stacker --debug --storage-type=overlay \
 	"--layer-type=squashfs" \
 	"--layer-type=tar" \
     "--substitute=PASST_VERSION=$PASST_VERSION" \
+    "--substitute=DOCKER_BASE=$DOCKER_BASE" \
+    "--substitute=UBUNTU_MIRROR=$UBUNTU_MIRROR" \
 	"--stacker-file=${TOP_D}/stacker.yaml"
