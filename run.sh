48bd7b5148398b3843f742ce1946d80727d859fc
Ryan Harper <rharper@woxford.com>
2023-03-08 15:06:26 -0600

    Create and Run a machine-network container with rootless CNIs

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+TOP_D=$PWD
+BUILD_D=${TOP_D}/build
+PASST_VERSION="2023_02_22.4ddbcb9"
+
+stacker --debug --storage-type=overlay \
+	"--oci-dir=$BUILD_D/oci" "--roots-dir=$BUILD_D/roots" "--stacker-dir=$BUILD_D/stacker" \
+	chroot  \
+    "--substitute=PASST_VERSION=$PASST_VERSION" \
+	"--stacker-file=${TOP_D}/machine-network.yaml"
